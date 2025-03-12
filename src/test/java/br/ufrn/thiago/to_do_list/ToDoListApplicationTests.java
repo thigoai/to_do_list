3176f7ea6253b0c40b1bfcc0f21b00e5bf47dde0
thigoai <tfreire352@gmail.com>
2025-03-11 21:38:41 -0300

    Primeiro commit sincronizado com GitHub

---
+++ src/test/java/br/ufrn/thiago/to_do_list/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.thiago.to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
