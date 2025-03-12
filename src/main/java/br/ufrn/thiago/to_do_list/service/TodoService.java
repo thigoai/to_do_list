5246ce3fc3282af8cc3bd37eb9de61dfb41fbf5c
thigoai <tfreire352@gmail.com>
2025-03-12 16:07:12 -0300

    adicionando os metodos dos serviços

--- src/main/java/br/ufrn/thiago/to_do_list/service/TodoService.java
+++ src/main/java/br/ufrn/thiago/to_do_list/service/TodoService.java
@@ -1,8 +1,41 @@
 package br.ufrn.thiago.to_do_list.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import br.ufrn.thiago.to_do_list.entity.Todo;
+import br.ufrn.thiago.to_do_list.repository.TodoRepository;
 
 @Service
 public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }    
     
+    public List<Todo> list(){
+        Sort sort = Sort.by("prioridade").descending().and(
+            Sort.by("nome").ascending()
+        );
+        return todoRepository.findAll(sort);
+    }    
+    
+    public List<Todo> uptade(Todo todo){
+        todoRepository.save(todo);
+        return list();        
+    }
+    
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }        
 }
