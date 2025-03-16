cf49659025d067268a38b9efd49a1f9a94e68609
thigoai <tfreire352@gmail.com>
2025-03-15 21:30:19 -0300

    adicionando os metodos do controlador

---
+++ src/main/java/br/ufrn/thiago/to_do_list/controller/TodoController.java
@@ -0,0 +1,47 @@
+package br.ufrn.thiago.to_do_list.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.ufrn.thiago.to_do_list.entity.Todo;
+import br.ufrn.thiago.to_do_list.service.TodoService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+    
+    @PostMapping
+    List<Todo> create(@RequestBody Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping 
+    List<Todo> list(){
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(Todo todo){
+        return todoService.uptade(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
+        return todoService.delete(id);
+    }
+}
